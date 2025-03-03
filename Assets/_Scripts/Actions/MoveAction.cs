eb93dd9492e31ecd8d6ca4be3dce95439b785d51
MikeHuijgen <mikeh162001@hotmail.com>
2025-03-03 14:41:16 +0100

    Made a new model to test out if it looks good also made it so the unit rotates towards movement

--- Assets/_Scripts/Actions/MoveAction.cs
+++ Assets/_Scripts/Actions/MoveAction.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class MoveAction : BaseAction
 {
     [SerializeField] private float speed;
+    [SerializeField] private float rotateSpeed;
     private Vector2 _moveDirection;
     private Rigidbody _rigidBody;
 
@@ -35,6 +36,15 @@ public class MoveAction : BaseAction
     private void FixedUpdate()
     {
         _rigidBody.linearVelocity = new Vector3(_moveDirection.x,0,_moveDirection.y) * speed * Time.fixedDeltaTime;
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        if (_moveDirection == Vector2.zero) return;
+        var targetRotation = Quaternion.LookRotation(new Vector3(_moveDirection.x, 0, _moveDirection.y));
+        targetRotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotateSpeed);
+        _rigidBody.MoveRotation(targetRotation);
     }
 
     public override void ActionGotInterrupted()
