148a1dd5557ac18e8b9a406535658432018afcc5
MikeHuijgen <mikeh162001@hotmail.com>
2025-05-21 14:44:17 +0200

    Made some classes we can use for the Eduana intergration

--- Assets/_Scripts/Temp/EduanaHandler.cs
+++ Assets/_Scripts/Temp/EduanaHandler.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEditor;
+using System.Collections.Generic;
 
 public class EduanaHandler : MonoBehaviour
 {
@@ -10,7 +11,7 @@ public class EduanaHandler : MonoBehaviour
     public void GetInfo()
     {
         //StartCoroutine(FetchKeywords(apiBase));
-        StartCoroutine(SendKeywords(apiBase));
+        //StartCoroutine(SendKeywords(apiBase));
     }
 
     private IEnumerator FetchKeywords(string apiBasePath)
@@ -31,29 +32,63 @@ public class EduanaHandler : MonoBehaviour
 
     }
 
-    private IEnumerator SendKeywords(string apiBasePath)
+    // private IEnumerator SendKeywords(string apiBasePath)
+    // {
+    //     //var newKeywords = new keywords();
+    //     var json = JsonUtility.ToJson(newKeywords);
+    //     print(newKeywords.keywordName);
+    //     var bytes = System.Text.Encoding.UTF8.GetBytes(json);
+    //     var request = new UnityWebRequest(apiBasePath, "PUT");
+
+
+    //     request.uploadHandler = new UploadHandlerRaw(bytes);
+    //     request.downloadHandler = new DownloadHandlerBuffer();
+    //     request.SetRequestHeader("Content-Type", "application/json");
+
+    //     yield return request.SendWebRequest();
+
+    //     if (request.result != UnityWebRequest.Result.Success)
+    //     {
+    //         Debug.LogError("Failed to send progress.");
+    //     }
+    // }
+}
+
+// public class keywords
+// {
+//     public string keywordName = "jsonBorn";
+// }
+
+
+//Method die aangroepen word als we keywords van de back-end willen hebben
+public class GetKeywords
+{
+    public Keyword[] keywords;
+}
+
+//The keyword class en de informatie
+public class Keyword
+{
+    public int id;
+    public string name;
+    public Question[] questions;
+
+    public Keyword(int id, string name, Question[] questions)
     {
-        var newKeywords = new keywords();
-        var json = JsonUtility.ToJson(newKeywords);
-        print(newKeywords.keywordName);
-        var bytes = System.Text.Encoding.UTF8.GetBytes(json);
-        var request = new UnityWebRequest(apiBasePath, "PUT");
-
-
-        request.uploadHandler = new UploadHandlerRaw(bytes);
-        request.downloadHandler = new DownloadHandlerBuffer();
-        request.SetRequestHeader("Content-Type", "application/json");
-
-        yield return request.SendWebRequest();
-
-        if (request.result != UnityWebRequest.Result.Success)
-        {
-            Debug.LogError("Failed to send progress.");
-        }
+        this.id = id;
+        this.name = name;
+        this.questions = questions;
     }
 }
 
-public class keywords
+public class Question
 {
-    public string keywordName = "jsonBorn";
+    public string text;
+    public Dictionary<string, Answer> answers;
+    public string correct_answer_id;
+}
+
+public class Answer
+{
+    public string text;
 }
